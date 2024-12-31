0fa6ec43881629b558ac5c0e55a8fafca9536dd0
Peter Stewart <peter@stewartmail.xyz>
2024-12-31 11:33:29 +0000

    Added deep sleep when off

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,20 @@
 #![no_main]
 use alloc::{boxed::Box, sync::Arc};
 use embassy_executor::Spawner;
-use embassy_futures::select;
+use embassy_futures::select::select;
+use embassy_sync::pubsub::{PubSubBehavior, PubSubChannel};
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, mutex::Mutex, signal::Signal};
 use embassy_time::{Delay, Duration, Instant, Timer};
-use embedded_storage::{ReadStorage, Storage};
 use esp_backtrace as _;
 use esp_hal::prelude::*;
+use esp_hal::rtc_cntl::sleep::RtcSleepConfig;
 use esp_hal::{
     gpio::{Input, Level, Output, Pull},
     i2c::master::{Config, I2c},
     timer::timg::TimerGroup,
 };
-use esp_println::println;
-use esp_storage::FlashStorage;
-use log::info;
+use log::{debug, info};
 use mpu6050::Mpu6050;
-use postcard::from_bytes;
-use serde::{Deserialize, Serialize};
 
 extern crate alloc;
 use core::mem::MaybeUninit;
@@ -44,54 +41,39 @@ fn init_heap() {
     }
 }
 
-type CalibratingSignal = Arc<Signal<CriticalSectionRawMutex, FlashData>>;
-type CalibratingBool = Arc<Mutex<CriticalSectionRawMutex, bool>>;
+type PowerSignal = PubSubChannel<CriticalSectionRawMutex, (), 1, 2, 1>;
+type PowerValue = Mutex<CriticalSectionRawMutex, bool>;
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
-struct FlashData {
-    acc_offset: [f32; 3],
-}
+static POWER_SIGNAL: PowerSignal = (PubSubChannel::<CriticalSectionRawMutex, (), 1, 2, 1>::new());
+static POWER_VALUE: PowerValue = Mutex::new(true);
+static MPU_READY: PowerSignal = (PubSubChannel::<CriticalSectionRawMutex, (), 1, 2, 1>::new());
 
 #[main]
 async fn main(_spawner: Spawner) {
     esp_println::logger::init_logger_from_env();
-    println!("Initialising...");
-
-    println!("Peripherals...");
-    let peripherals = esp_hal::init(esp_hal::Config::default());
-
-    println!("Heap...");
+    info!("Initialising...");
+    info!("Heap...");
     init_heap();
 
-    println!("Embassy...");
+    info!("Peripherals...");
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+
+    info!("Embassy...");
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_hal_embassy::init(timg0.timer0);
 
-    println!("GPIO...");
-    let mut led = Output::new(peripherals.GPIO0, Level::Low);
-    let mut vibration_motor = Output::new(peripherals.GPIO26, Level::Low);
-    let calibration_button = Input::new(peripherals.GPIO27, Pull::Up);
+    info!("GPIO...");
+    let mut mpu_power = Output::new(peripherals.GPIO23, Level::High);
+    let led: Arc<Mutex<CriticalSectionRawMutex, Output>> =
+        Arc::new(Mutex::new(Output::new(peripherals.GPIO22, Level::Low)));
+    let vibration_motor = Arc::new(Mutex::new(Output::new(peripherals.GPIO26, Level::Low)));
+    let power_button = Input::new(peripherals.GPIO5, Pull::None);
 
-    println!("Flash...");
-    // Initialize the ESP32 flash memory to store the calibration data
-    let mut flash = FlashStorage::new();
+    // Wait for the MPU6050 to power up
+    Timer::after(Duration::from_millis(100)).await;
 
-    let flash_addr = 0x9000;
-    println!("Flash size = {}", flash.capacity());
-    let mut data = FlashData {
-        acc_offset: [0.0, 0.0, 0.0],
-    };
-
-    // Read the calibration data from flash
-    let mut buffer = [0u8; core::mem::size_of::<FlashData>()];
-    if let Ok(()) = flash.read(flash_addr, &mut buffer) {
-        data = from_bytes(&buffer).unwrap();
-    }
-
-    println!("Read flash data: {:?}", data.acc_offset);
-
-    // Initialize the accscope (MPU6050)
-    let accscope = I2c::new(
+    // Initialize the accelerometer (MPU6050)
+    let accelerometer = I2c::new(
         peripherals.I2C0,
         Config {
             frequency: fugit::Rate::<u32, 1, 1>::kHz(100),
@@ -101,155 +83,175 @@ async fn main(_spawner: Spawner) {
     .with_sda(peripherals.GPIO33)
     .with_scl(peripherals.GPIO32);
 
-    let acc_interrupt = Input::new((peripherals.GPIO25), Pull::Up);
-
-    let mut delay = Delay;
     let mpu: Arc<Mutex<CriticalSectionRawMutex, Mpu6050<I2c<'_, esp_hal::Blocking>>>> =
-        Arc::new(Mutex::new(Mpu6050::new(accscope)));
-
-    {
-        let mut mpu = mpu.lock().await;
-        mpu.init(&mut delay).expect("MPU6050 init failed");
-
-        mpu.set_accel_range(mpu6050::device::AccelRange::G16)
-            .expect("set_acc_range failed");
-    }
-    // Vibrate twice to tell the user that the device is ready
-    led.set_high();
-    vibration_motor.set_high();
-    Timer::after(Duration::from_millis(100)).await;
-    led.set_low();
-    vibration_motor.set_low();
-
-    Timer::after(Duration::from_millis(100)).await;
-
-    led.set_high();
-    vibration_motor.set_high();
-    Timer::after(Duration::from_millis(100)).await;
-    vibration_motor.set_low();
-    led.set_low();
-
-    let calibrate_signal: CalibratingSignal =
-        Arc::new(Signal::<CriticalSectionRawMutex, FlashData>::new());
-
-    let calibrating: CalibratingBool = Arc::new(Mutex::new(false));
+        Arc::new(Mutex::new(Mpu6050::new(accelerometer)));
 
     // Start the calibration task
-    _spawner
-        .spawn(calibrate(
-            Box::new(calibration_button),
-            calibrate_signal.clone(),
-            calibrating.clone(),
-            mpu.clone(),
-            flash,
-            data.clone(),
-        ))
-        .unwrap();
+    _spawner.spawn(handle_power_button(power_button)).unwrap();
 
     _spawner
         .spawn(check_orientation_and_vibrate(
             mpu.clone(),
-            Box::new(acc_interrupt),
-            Box::new(vibration_motor),
-            calibrate_signal.clone(),
-            calibrating.clone(),
-            data,
+            vibration_motor.clone(),
         ))
         .unwrap();
+
+    let mut fef = RtcSleepConfig::default();
+    let led_clone = led.clone();
+    let vib = vibration_motor.clone();
+    let mut sub = POWER_SIGNAL.subscriber().unwrap();
+    loop {
+        let power = { *POWER_VALUE.lock().await };
+
+        if power {
+            info!("Waking up");
+            fef.set_deep_slp(false);
+            info!("Setting MPU power high");
+            mpu_power.set_high();
+            info!("Waiting for MPU to power up");
+            Timer::after(Duration::from_millis(100)).await;
+
+            info!("Initialising MPU");
+            let delay = Delay;
+            init_mpu(mpu.clone(), delay).await;
+
+            info!("Device ready");
+
+            // Vibrate twice to tell the user that the device is ready
+            {
+                let mut led = led_clone.lock().await;
+                let mut vibration_motor = vib.lock().await;
+                led.set_low();
+                vibration_motor.set_high();
+            }
+            Timer::after(Duration::from_millis(100)).await;
+            {
+                let mut led = led_clone.lock().await;
+                let mut vibration_motor = vib.lock().await;
+                led.set_high();
+                vibration_motor.set_low();
+            }
+
+            Timer::after(Duration::from_millis(100)).await;
+
+            {
+                let mut led = led_clone.lock().await;
+                let mut vibration_motor = vib.lock().await;
+                led.set_low();
+                vibration_motor.set_high();
+            }
+            Timer::after(Duration::from_millis(100)).await;
+            {
+                let mut led = led_clone.lock().await;
+                let mut vibration_motor = vib.lock().await;
+                vibration_motor.set_low();
+                led.set_high();
+            }
+        } else {
+            info!("Going to sleep");
+            // Set ESP32 to low power mode
+            mpu_power.set_low();
+            {
+                let mut led = led_clone.lock().await;
+                let mut vibration_motor = vib.lock().await;
+                vibration_motor.set_low();
+                led.set_high();
+            }
+            fef.set_deep_slp(true);
+        }
+
+        info!("Waiting for power signal");
+
+        sub.next_message().await;
+
+        info!("Power signal received");
+    }
+}
+
+async fn init_mpu(
+    mpu: Arc<Mutex<CriticalSectionRawMutex, Mpu6050<I2c<'_, esp_hal::Blocking>>>>,
+    mut delay: Delay,
+) {
+    let mut mpu = mpu.lock().await;
+    mpu.init(&mut delay).expect("MPU6050 init failed");
+
+    // mpu.set_accel_range(mpu6050::device::AccelRange::G16)
+    //     .expect("set_acc_range failed");
+
+    mpu.set_temp_enabled(true).expect("set_temp_enabled failed");
+
+    // mpu.set_clock_source(mpu6050::device::CLKSEL::EXT_32p7)
+    //     .expect("set_clock_source failed");
+
+    // mpu.set_gyro_range(GyroRange::D250)
+    //     .expect("set_gyro_range failed");
+
+    info!("MPU6050 initialized");
+
+    MPU_READY.publish_immediate(());
 }
 
 #[embassy_executor::task]
-async fn calibrate(
-    mut calibration_button: Box<Input<'static>>,
-    calibrating_finished: CalibratingSignal,
-    calibrating: CalibratingBool,
-    mpu: Arc<Mutex<CriticalSectionRawMutex, Mpu6050<I2c<'static, esp_hal::Blocking>>>>,
-    mut flash: FlashStorage,
-    mut data: FlashData,
-) {
-    // Wait for the calibration button to be pressed
-    calibration_button.wait_for_high().await;
+async fn handle_power_button(mut power_button: Input<'static>) {
+    loop {
+        // Wait for the power button to be pressed
+        power_button.wait_for_rising_edge().await;
 
-    {
-        let mut calibrating = calibrating.lock().await;
-        *calibrating = true;
-    }
-
-    // Wait for the calibration button to be released
-    calibration_button.wait_for_low().await;
-
-    // Wait for 1 second to allow the user to move their hands away from the device
-    Timer::after(Duration::from_secs(1)).await;
-
-    // Calibrate the accscope, assuming that the device is stationary and lying flat on its back
-    {
-        let mut mpu = mpu.lock().await;
-        let mut acc_sum = [0.0, 0.0, 0.0];
-
-        for _ in 0..100 {
-            let acc = mpu.get_acc().unwrap();
-            acc_sum[0] += acc[0];
-            acc_sum[1] += acc[1];
-            acc_sum[2] += acc[2];
-
-            Timer::after(Duration::from_millis(100)).await;
+        info!("Power button pressed");
+        {
+            let mut val = POWER_VALUE.lock().await;
+            *val = !*val;
         }
+        POWER_SIGNAL.publish_immediate(());
+        power_button.wait_for_falling_edge().await;
 
-        data.acc_offset[0] = acc_sum[0] / 100.0;
-        data.acc_offset[1] = acc_sum[1] / 100.0;
-        data.acc_offset[2] = acc_sum[2] / 100.0;
+        info!("Power button released");
 
-        // Write the calibration data to flash
-        let buffer = postcard::to_allocvec(&data).unwrap();
-        flash.write(0x9000, &buffer).unwrap();
-
-        info!("Calibration data: {:?}", data.acc_offset);
+        Timer::after(Duration::from_millis(500)).await;
     }
-
-    // Signal that the calibration is finished
-    {
-        let mut calibrating = calibrating.lock().await;
-        *calibrating = false;
-    }
-    calibrating_finished.signal(data);
 }
 
 #[embassy_executor::task]
 async fn check_orientation_and_vibrate(
     mpu: Arc<Mutex<CriticalSectionRawMutex, Mpu6050<I2c<'static, esp_hal::Blocking>>>>,
-    mut acc_interrupt: Box<Input<'static>>,
-    mut vibration_motor: Box<Output<'static>>,
-    calibrating_finished: CalibratingSignal,
-    calibrating: CalibratingBool,
-    data: FlashData,
+    vibration_motor: Arc<Mutex<CriticalSectionRawMutex, Output<'static>>>,
 ) {
     // If the device is upside down and has been for 5 seconds, vibrate in increasing intervals and intensities
     // Otherwise the device is not upside down, turn off the vibration motor
     let mut upside_down_since: Option<Instant> = None;
 
-    let mut data = data.clone();
-
-    // Setup motion detection
-    {
-        let mut mpu = mpu.lock().await;
-        mpu.setup_motion_detection().unwrap();
-    }
+    let mut mpu_ready = MPU_READY.subscriber().unwrap();
+    let mut power_signal = POWER_SIGNAL.subscriber().unwrap();
 
     loop {
-        let is_calibrating = { *(calibrating.lock().await) };
-        if is_calibrating {
-            // Wait for the calibration to finish
-            data = calibrating_finished.wait().await;
+        {
+            info!("Getting power value");
+            let val = { *POWER_VALUE.lock().await };
+            if !val {
+                info!("Power off");
+
+                loop {
+                    let power = { *POWER_VALUE.lock().await };
+                    if power {
+                        mpu_ready.next_message().await;
+                        info!("MPU ready");
+                        break;
+                    }
+                    power_signal.next_message().await;
+                }
+                info!("Power on");
+            }
         }
 
-        let mut mpu = mpu.lock().await;
-
-        // Read the accscope data
-        let acc = mpu.get_acc().unwrap();
+        let acc = {
+            let mut mpu = mpu.lock().await;
+            // Read the accelerometer data
+            mpu.get_acc().unwrap_or([0.0, 0.0, 0.0].into())
+        };
 
         // Determine if the device is upside down
         let z_acc = acc[2];
-        let is_upside_down = z_acc < -5.0;
+        let is_upside_down = z_acc < -0.6;
 
         info!("Acc: {:?}", z_acc);
         if is_upside_down {
@@ -266,29 +268,22 @@ async fn check_orientation_and_vibrate(
         if let Some(since) = upside_down_since {
             if Instant::now() - since > Duration::from_secs(5) {
                 info!("Upside down for 5 seconds, vibrating");
-                vibration_motor.set_high();
+                {
+                    let mut vibration_motor = vibration_motor.lock().await;
+                    vibration_motor.set_high();
+                }
                 Timer::after(Duration::from_millis(1000)).await;
-                vibration_motor.set_low();
+                {
+                    let mut vibration_motor = vibration_motor.lock().await;
+                    vibration_motor.set_low();
+                }
                 upside_down_since = None;
             }
         }
 
-        // Wait for the next interrupt, or 5s, whichever comes first
-        let sig = acc_interrupt.wait_for_low();
+        let power_sig = power_signal.next_message();
+        let timeout = Timer::after(Duration::from_millis(5000));
 
-        let timer = Timer::after(Duration::from_millis(5000));
-
-        let res = select::select(sig, timer).await;
-
-        match res {
-            select::Either::First(_) => {
-                // Acc interrupt
-                info!("Acc interrupt");
-            }
-            select::Either::Second(_) => {
-                // Timeout
-                info!("Timeout");
-            }
-        }
+        select(power_sig, timeout).await;
     }
 }
